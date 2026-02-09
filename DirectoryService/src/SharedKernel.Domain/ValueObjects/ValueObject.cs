1f677d3f0beb173c06398fa4d702a7834f114d83
proninp <velninpapro@gmail.com>
2026-02-09 22:04:19 +0300

    Added SharedKernel project; Added ValueObject; Added some fixes to editorconfig;

---
+++ DirectoryService/src/SharedKernel.Domain/ValueObjects/ValueObject.cs
@@ -0,0 +1,50 @@
+﻿namespace SharedKernel.Domain.ValueObjects;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        var other = (ValueObject)obj;
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return GetEqualityComponents()
+                .Aggregate(17, (hash, obj) => hash * 31 + obj?.GetHashCode() ?? 0);
+        }
+    }
+
+    public static bool operator ==(ValueObject? left, ValueObject? right)
+    {
+        if (left is null && right is null)
+        {
+            return true;
+        }
+
+        if (left is null || right is null)
+        {
+            return false;
+        }
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ValueObject? left, ValueObject? right) =>
+        !(left == right);
+}
