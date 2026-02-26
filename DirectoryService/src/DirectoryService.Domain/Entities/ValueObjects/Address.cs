9bc3840cc4c3b56d8bdc5aa0182fa2ba585f075d
proninp <velninpapro@gmail.com>
2026-02-26 21:38:06 +0300

    Added Value objects

--- DirectoryService/src/DirectoryService.Domain/Entities/ValueObjects/Address.cs
+++ DirectoryService/src/DirectoryService.Domain/Entities/ValueObjects/Address.cs
@@ -1,20 +1,98 @@
+using CSharpFunctionalExtensions;
+using DirectoryService.Domain.Common;
+
 namespace DirectoryService.Domain.Entities.ValueObjects;
 
 public sealed record Address
 {
-    public string? PostalCode { get; init; } // 10 symb
+    private const int PostalCodeMinLength = 2;
 
-    public string? Country { get; init; } // 150 symb
+    private const int PostalCodeMaxLength = 10;
 
-    public string? City { get; init; } // 150 symb
+    private const int CountryMinLength = 2;
 
-    public string? Street { get; init; } // 150 symb
+    private const int CountryMaxLength = 50;
 
-    public string? House { get; init; } // 50 symb
+    private const int CityMinLength = 3;
 
-    public string? Block { get; init; } // 50 symb
+    private const int CityMaxLength = 50;
 
-    public string? Room { get; init; } // 50 symb
+    private const int StreetMinLength = 3;
 
-    public string? PostalBox { get; init; } // 50 symb
+    private const int StreetMaxLength = 100;
+
+    private const int HouseMinLength = 1;
+
+    private const int HouseMaxLength = 50;
+
+    private const int BlockMinLength = 1;
+
+    private const int BlockMaxLength = 10;
+
+    private const int RoomMinLength = 1;
+
+    private const int RoomMaxLength = 10;
+
+    private const int PostalBoxMinLength = 1;
+
+    private const int PostalBoxMaxLength = 20;
+
+    public string PostalCode { get; }
+
+    public string Country { get; }
+
+    public string City { get; }
+
+    public string Street { get; }
+
+    public string House { get; }
+
+    public string? Block { get; }
+
+    public string? Room { get; }
+
+    public string? PostalBox { get; }
+
+    private Address(string postalCode, string country, string city, string street, string house,
+        string? block = null, string? room = null, string? postalBox = null
+    )
+    {
+        PostalCode = postalCode;
+        Country = country;
+        City = city;
+        Street = street;
+        House = house;
+        Block = block;
+        Room = room;
+        PostalBox = postalBox;
+    }
+
+    public static Result<Address> Create(string postalCode, string country, string city, string street, string house,
+        string? block = null, string? room = null, string? postalBox = null
+    )
+    {
+        var validationResult = Result.Combine(
+            Guard.ValidateStringField(postalCode, nameof(postalCode), PostalCodeMinLength, PostalCodeMaxLength),
+            Guard.ValidateStringField(country, nameof(country), CountryMinLength, CountryMaxLength),
+            Guard.ValidateStringField(city, nameof(city), CityMinLength, CityMaxLength),
+            Guard.ValidateStringField(street, nameof(street), StreetMinLength, StreetMaxLength),
+            Guard.ValidateStringField(house, nameof(house), HouseMinLength, HouseMaxLength),
+            string.IsNullOrEmpty(block)
+                ? Result.Success()
+                : Guard.ValidateStringField(block, nameof(block), BlockMinLength, BlockMaxLength),
+            string.IsNullOrEmpty(room)
+                ? Result.Success()
+                : Guard.ValidateStringField(room, nameof(room), RoomMinLength, RoomMaxLength),
+            string.IsNullOrEmpty(postalBox)
+                ? Result.Success()
+                : Guard.ValidateStringField(postalBox, nameof(postalBox), PostalBoxMinLength, PostalBoxMaxLength)
+        );
+
+        if (validationResult.IsFailure)
+        {
+            return Result.Failure<Address>(validationResult.Error);
+        }
+
+        return new Address(postalCode, country, city, street, house, block, room, postalBox);
+    }
 }
---
+++ DirectoryService/src/DirectoryService.Domain/Entities/ValueObjects/Path.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.RegularExpressions;
+using CSharpFunctionalExtensions;
+using DirectoryService.Domain.Common;
+
+namespace DirectoryService.Domain.Entities.ValueObjects;
+
+public sealed record Path
+{
+    private const int MinLength = 2;
+
+    private const int MaxLength = 500;
+
+    private const char Separator = '.';
+
+    public string Value { get; }
+
+    private Path(string value) => Value = value;
+
+    public static Result<Path> Create(string path)
+    {
+        if (!string.IsNullOrEmpty(path))
+        {
+            path = Regex.Replace(path.Trim(), @"\s+", " ");
+        }
+
+        var validationResult = Guard.ValidateStringField(path, nameof(path), MinLength, MaxLength);
+        if (validationResult.IsFailure)
+        {
+            return Result.Failure<Path>(validationResult.Error);
+        }
+
+        path = new string([.. path.Select(c => char.IsPunctuation(c) ? '_' : c)]);
+
+        return new Path(path);
+    }
+
+    public static Result<Path> CreateForChild(Path parentPath, Identifier childIdentifier)
+    {
+        var fullPath = $"{parentPath.Value}{Separator}{childIdentifier.Value}";
+
+        return Create(fullPath);
+    }
+}
