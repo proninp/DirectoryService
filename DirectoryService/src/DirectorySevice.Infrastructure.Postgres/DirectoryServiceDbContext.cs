086738440f7157c249eb84baf24a800fd3382d8b
proninp <velninpapro@gmail.com>
2026-03-09 17:34:46 +0300

    Added DbContext stuff

--- DirectoryService/src/DirectorySevice.Infrastructure.Postgres/DirectoryServiceDbContext.cs
+++ DirectoryService/src/DirectorySevice.Infrastructure.Postgres/DirectoryServiceDbContext.cs
@@ -1,7 +1,29 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DirectoryService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DirectorySevice.Infrastructure.Postgres;
 
 public class DirectoryServiceDbContext : DbContext
 {
+    private readonly string _connectionString;
+
+    protected DirectoryServiceDbContext(string connectionString) => _connectionString = connectionString;
+
+    public DbSet<Department> Departments => Set<Department>();
+
+    public DbSet<Location> Locations => Set<Location>();
+
+    public DbSet<Position> Positions => Set<Position>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseNpgsql(_connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DirectoryServiceDbContext).Assembly);
+    }
 }
