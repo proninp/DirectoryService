4cbbc229a83091a5fb97dc3476e39909ffee8f7d
proninp <velninpapro@gmail.com>
2026-04-08 10:16:42 +0300

    Added GlobalExceptionHandler;

---
+++ DirectoryService/src/DirectoryService.App/Middlewares/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace DirectoryService.App.Middlewares;
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static IServiceCollection AddGlobalExceptionHandling(this IServiceCollection services)
+    {
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+        services.AddProblemDetails();
+        return services;
+    }
+}
---
+++ DirectoryService/src/DirectoryService.App/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DirectoryService.App.Middlewares;
+
+public sealed class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger;
+
+    public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception,
+        CancellationToken cancellationToken)
+    {
+        _logger.LogError(
+            exception, "Exception occurred: {Message}", exception.Message);
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Title = "Server Error"
+        };
+
+        httpContext.Response.StatusCode =
+            StatusCodes.Status500InternalServerError;
+
+        httpContext.Response.ContentType = "application/json";
+        await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+        return true;
+    }
+}
