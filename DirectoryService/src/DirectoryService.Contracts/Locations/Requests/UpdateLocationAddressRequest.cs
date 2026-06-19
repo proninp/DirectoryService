857de786a1a777d0b7df7cd7fa917234741aa622
proninp <velninpapro@gmail.com>
2026-06-19 22:51:46 +0300

    Added dixes to Address entity as well as in Location and Address contracts;

--- DirectoryService/src/DirectoryService.Contracts/Locations/Requests/UpdateLocationAddressRequest.cs
+++ DirectoryService/src/DirectoryService.Contracts/Locations/Requests/UpdateLocationAddressRequest.cs
@@ -1,12 +1,45 @@
-﻿namespace DirectoryService.Contracts.Locations.Requests;
+﻿using CSharpFunctionalExtensions;
+using DirectoryService.Domain.Entities.ValueObjects;
+using DirectoryService.Shared;
+
+namespace DirectoryService.Contracts.Locations.Requests;
 
 public record UpdateLocationAddressRequest(
-    string PostalCode,
-    string Country,
-    string City,
-    string Street,
-    string House,
+    string? PostalCode,
+    string? Country,
+    string? City,
+    string? Street,
+    string? House,
     string? Block,
     string? Room,
     string? PostalBox
 );
+
+public static class UpdateLocationAddressRequestExtensions
+{
+    public static Result<Address, Errors> ToAddress(this UpdateLocationAddressRequest request)
+    {
+        return Address.Create(
+            request.PostalCode!,
+            request.Country!,
+            request.City!,
+            request.Street!,
+            request.House!,
+            request.Block,
+            request.Room,
+            request.PostalBox);
+    }
+
+    public static Result<Address, Errors> ToAddress(this UpdateLocationAddressRequest request, Address address)
+    {
+        return address.With(
+            request.PostalCode,
+            request.Country,
+            request.City,
+            request.Street,
+            request.House,
+            request.Block,
+            request.Room,
+            request.PostalBox);
+    }
+}
--- DirectoryService/src/DirectoryService.Contracts/Locations/Requests/UpdateLocationRequest.cs
+++ DirectoryService/src/DirectoryService.Contracts/Locations/Requests/UpdateLocationRequest.cs
@@ -1,7 +1,7 @@
 ﻿namespace DirectoryService.Contracts.Locations.Requests;
 
 public record UpdateLocationRequest(
-    string Name,
-    UpdateLocationAddressRequest AddressRequest,
-    string Timezone
+    string? Name,
+    UpdateLocationAddressRequest? AddressRequest,
+    string? Timezone
 );
