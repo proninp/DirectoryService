972505d74c97faaf1145c143db01e3386ff3c341
proninp <velninpapro@gmail.com>
2026-07-13 21:59:20 +0300

    Added Query model; Added Controller method;

---
+++ DirectoryService/src/DirectoryService.Application/Locations/GetLocation/GetLocationListQuery.cs
@@ -0,0 +1,12 @@
+﻿using DirectoryService.Application.Abstractions;
+using DirectoryService.Contracts.Locations.Requests;
+
+namespace DirectoryService.Application.Locations.GetLocation;
+
+public record GetLocationListQuery(GetLocationListRequest Request) : IQuery;
+
+public static class GetLocationListQueryExtensions
+{
+    public static GetLocationListQuery ToQuery(this GetLocationListRequest request) =>
+        new(request);
+}
