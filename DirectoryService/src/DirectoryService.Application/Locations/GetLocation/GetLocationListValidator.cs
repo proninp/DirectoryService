3a2ece0490e19f8e00dfd8244e0eed0eb7e6d2aa
proninp <velninpapro@gmail.com>
2026-07-13 22:40:43 +0300

    Added GetLocationListValidator; Fixed NameLength consts in Department entity; Added AllowedSortDir to CustomValidators;

--- DirectoryService/src/DirectoryService.Application/Locations/GetLocation/GetLocationListValidator.cs
+++ DirectoryService/src/DirectoryService.Application/Locations/GetLocation/GetLocationListValidator.cs
@@ -1,42 +1,43 @@
 ﻿using DirectoryService.Application.Validation;
 using DirectoryService.Contracts.Departments.Requests;
+using DirectoryService.Contracts.Locations.Requests;
 using DirectoryService.Contracts.Pagination;
 using DirectoryService.Domain.Entities;
 using DirectoryService.Shared;
 using FluentValidation;
 
-namespace DirectoryService.Application.Departments.GetDepartment;
+namespace DirectoryService.Application.Locations.GetLocation;
 
-public sealed class GetDepartmentListValidator : AbstractValidator<GetDepartmentListQuery>
+public sealed class GetLocationListValidator : AbstractValidator<GetLocationListQuery>
 {
     private static readonly HashSet<string> AllowedSortBy = new(StringComparer.OrdinalIgnoreCase)
-        { nameof(Department.Name), nameof(Department.CreatedAt) };
+        { nameof(Location.Name), nameof(Location.CreatedAt), nameof(GetLocationListQuery.Request.MinDepartmentCount) };
 
-    public GetDepartmentListValidator()
+    public GetLocationListValidator()
     {
         RuleFor(q => q.Request)
             .NotNull()
-            .WithError(GeneralErrors.ValueIsRequired(nameof(GetDepartmentListQuery.Request)))
+            .WithError(GeneralErrors.ValueIsRequired(nameof(GetLocationListQuery.Request)))
             .DependentRules(() =>
             {
                 RuleFor(q => q.Request.Search)
-                    .MaximumLength(Department.NameMaxLength)
+                    .MaximumLength(Location.NameMaxLength)
                     .WithError(GeneralErrors.InvalidFieldLength(
-                        nameof(GetDepartmentListRequest.Search), maxLength: Department.NameMaxLength));
+                        nameof(GetLocationListRequest.Search), maxLength: Location.NameMaxLength));
 
                 RuleFor(query => query.Request.SortBy)
                     .Must(sortBy => string.IsNullOrWhiteSpace(sortBy) ||
                                     AllowedSortBy.Contains(sortBy))
-                    .WithError(GeneralErrors.ValueIsInvalid(nameof(GetDepartmentListRequest.SortBy)));
+                    .WithError(GeneralErrors.ValueIsInvalid(nameof(GetLocationListRequest.SortBy)));
 
                 RuleFor(query => query.Request.SortDir)
                     .Must(sortDir => string.IsNullOrWhiteSpace(sortDir) ||
                                      CustomValidators.AllowedSortDir.Contains(sortDir))
-                    .WithError(GeneralErrors.ValueIsInvalid(nameof(GetDepartmentListRequest.SortDir)));
+                    .WithError(GeneralErrors.ValueIsInvalid(nameof(GetLocationListRequest.SortDir)));
 
                 RuleFor(query => query.Request.Pagination)
                     .NotNull()
-                    .WithError(GeneralErrors.ValueIsRequired(nameof(GetDepartmentListRequest.Pagination)))
+                    .WithError(GeneralErrors.ValueIsRequired(nameof(GetLocationListRequest.Pagination)))
                     .DependentRules(() =>
                     {
                         RuleFor(query => query.Request.Pagination.PageNumber)
