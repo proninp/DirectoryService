9d0cb72fa65105d1b5a1a2327cc0f2a264d6724f
proninp <velninpapro@gmail.com>
2026-07-13 21:39:02 +0300

    Added LocationListItemResponse as response model; Added GetLocationListRequest as request model;

---
+++ DirectoryService/src/DirectoryService.Contracts/Locations/Requests/GetLocationListRequest.cs
@@ -0,0 +1,10 @@
+﻿using DirectoryService.Contracts.Pagination;
+
+namespace DirectoryService.Contracts.Locations.Requests;
+
+public record GetLocationListRequest(
+    string? Search,
+    int MinDepartmentCount,
+    string? SortBy,
+    string? SortDir,
+    PagedRequest Pagination);
---
+++ DirectoryService/src/DirectoryService.Contracts/Locations/Responses/LocationListItemResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace DirectoryService.Contracts.Locations.Responses;
+
+public record LocationListItemResponse(
+    Guid Id,
+    string Name,
+    string PostalCode,
+    string Country,
+    string City,
+    string Street,
+    string House,
+    string? Block,
+    string? Room,
+    string? PostalBox,
+    DateTime CreatedAt,
+    int DepartmentCount);
